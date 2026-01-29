d1d6eb866952206d2843a60d7dcadaa3b82dc164
Bharath200415 <bharath200415@gmail.com>
2026-01-29 19:50:37 +0530

    basic rate limiter

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require("express");
+const ip = require("ip");
+
+const MAX_Allowed = 5;
+const MAX_Time = 20000; //30s 
+
+let ip_mapping = {
+
+}
+
 const app = express();
+
+setInterval(()=>{
+    ip_mapping={};
+    console.log("IP_mapping cleared");
+
+},MAX_Time)
+app.use((req,res,next)=>{
+    
+    const my_ip=req.ip;
+    const server_ip = ip.address();
+    ip_mapping[my_ip] = ip_mapping[my_ip]+1 || 1;
+
+    if (ip_mapping[my_ip]>MAX_Allowed){
+        console.error("too many requests")
+        console.log(`received request number ${ip_mapping[my_ip]} from ${server_ip}`);
+        return res.status(429).send("too many request");
+    }
+    next();
+
+})
 
 app.get("/",(req,res)=>{
     console.log("received a request");
-
     res.status(200).send("ok");
+
 })
+
+
+
 app.listen(3000,()=>{
     console.log("running on port 3000")
 })
